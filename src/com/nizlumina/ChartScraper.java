890942fe85a01a0e0aac453325b0e4fdd5f8ec41
Nizlumina <nizlumina@gmail.com>
2015-02-04 12:30:34 +0800

    Initial commit

---
+++ src/com/nizlumina/ChartScraper.java
@@ -0,0 +1,19 @@
+package com.nizlumina;
+
+public class ChartScraper
+{
+    public enum Season{
+        SPRING, SUMMER, FALL, WINTER
+    }
+
+    public void scrape(Season season){
+
+    }
+
+    public class ScrapeObject
+    {
+        String name;
+        String link;
+        String category;
+    }
+}
---
+++ src/com/nizlumina/Main.java
@@ -0,0 +1,8 @@
+package com.nizlumina;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
