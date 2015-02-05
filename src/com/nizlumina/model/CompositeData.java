145c3971373bf637e091a12f540c58ced37ad80a
Nizlumina <nizlumina@gmail.com>
2015-02-06 00:52:59 +0800

    Production version commit

--- src/com/nizlumina/model/CompositeData.java
+++ src/com/nizlumina/model/CompositeData.java
@@ -1,11 +1,66 @@
 package com.nizlumina.model;
 
+import com.nizlumina.model.hummingbird.v2.AnimeObject;
+
 /**
  * Composite data that wraps each subclasses
  */
 public class CompositeData
 {
-    AnimeObject animeObject;
+    private int id;
+    private MALObject malObject;
+    private AnimeObject animeObject;
+    private LiveChartObject liveChartObject;
 
+    public CompositeData(int id, MALObject malObject, AnimeObject animeObject, LiveChartObject liveChartObject)
+    {
+        this.id = id;
+        this.malObject = malObject;
+        this.animeObject = animeObject;
+        this.liveChartObject = liveChartObject;
+    }
 
+    public CompositeData()
+    {
+    }
+
+    public LiveChartObject getLiveChartObject()
+    {
+        return liveChartObject;
+    }
+
+    public void setLiveChartObject(LiveChartObject liveChartObject)
+    {
+        this.liveChartObject = liveChartObject;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public MALObject getMalObject()
+    {
+        return malObject;
+    }
+
+    public void setMalObject(MALObject malObject)
+    {
+        this.malObject = malObject;
+    }
+
+    public AnimeObject getAnimeObject()
+    {
+        return animeObject;
+    }
+
+    public void setAnimeObject(AnimeObject animeObject)
+    {
+        this.animeObject = animeObject;
+    }
 }
